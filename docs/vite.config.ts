c19b8d235ab24d99f9305157c12337cfb28950fb
Erick Rost <erickrosts@gmail.com>
2026-04-24 00:48:18 -0300

    build

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/ra2581392523028/',
+  build: {
+    outDir: '.',
+    emptyOutDir: false,
+  }
 })
